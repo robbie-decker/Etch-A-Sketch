3e2230cc1134d7a2af4667d3af0999db4bfdaba3
Robbie Decker <robbd56@gmail.com>
2023-06-07 14:48:38 -0400

    Add grid clearing functionality

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 const grid = document.getElementById("grid");
 const row = document.createElement("div")
 const block = document.createElement("div");
+const clear = document.getElementById("clear");
+
 
 row.className = "row";
 block.className = "block";
 grid.addEventListener("mouseover", (e) => {
     if(e.target.className == "block"){
         e.target.style.backgroundColor = "red";
-        console.log(e);
     }
 });
-
 
 const test = document.getElementById("test");
 test.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "red";
-    console.log(e);
+});
+
+
+clear.addEventListener("click", () => {
+    clearGrid();
+    buildGrid(sizeX, sizeY);
 });
 
 
 
-sizeX = 8;
+function buildGrid(sizeX, sizeY){
+    for(let x = 0; x < sizeX; x++){
+        // cloneNode(true) allows us to have multiple of the same element be appended
+        row.appendChild(block.cloneNode(true));
+    }
+    
+    for(let y = 0; y < sizeY; y++){
+        grid.appendChild(row.cloneNode(true));
+    }
+}
+
+function clearGrid(){
+    grid.replaceChildren();
+    row.replaceChildren();
+}
+
+
 sizeY = 8;
+sizeX = 8;
 
-for(let x = 0; x < sizeX; x++){
-    // cloneNode(true) allows us to have multiple of the same element be appended
-    row.appendChild(block.cloneNode(true));
-}
-
-for(let y = 0; y < sizeY; y++){
-    grid.appendChild(row.cloneNode(true));
-}
-
+buildGrid(sizeX, sizeY);
