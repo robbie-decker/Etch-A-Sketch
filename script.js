6e5861b9f18403a1afbf8dc7ce80ec198506a0b1
Robbie Decker <robbd56@gmail.com>
2023-06-13 23:39:28 -0400

    Rainbow functionality and shake when clearing
    The shake took forever to do and this should have been two commits

--- script.js
+++ script.js
@@ -3,26 +3,66 @@ const row = document.createElement("div")
 const block = document.createElement("div");
 const clear = document.getElementById("clear");
 
+
 let sizeSelect = document.getElementById("size");
+let colorSelect = document.querySelector('input[type="color"]');
+let rainbowToggle = document.querySelector('input[type="checkbox"]')
 
 row.className = "row";
 block.className = "block";
+size = sizeSelect.value;
+
+buildGrid(size);
+
+let color;
 grid.addEventListener("mouseover", (e) => {
+    if(e.target.className == "block blockTransition"){
+        e.target.classList.remove("blockTransition");
+    }
     if(e.target.className == "block"){
-        e.target.style.backgroundColor = "red";
+        if(rainbowToggle.checked){
+            color = generateRainbowColor();
+            e.target.style.backgroundColor = color;
+        }
+        else{
+            color = colorSelect.value;
+            e.target.style.backgroundColor = color;
+        }
     }
 });
 
 sizeSelect.addEventListener("change", (e) => {
     size = sizeSelect.value;
-    clearGrid();
+    removeGrid();
     buildGrid(size);
 });
 
+const gridShake = {
+        'marginLeft' : ["0px", "-10px", "10px", "-20px", "20px", "-20px", "20px", "-20px", "20px", "0px"],
+    };
+    
+const gridTiming = {
+    duration: 1000,
+    easing: "ease-in-out",
+    iterations: 1,
+};
+
+// Make the grid shake when click is clicked
 clear.addEventListener("click", () => {
+    grid.animate(gridShake, gridTiming);
     clearGrid();
-    buildGrid(size);
 });
+
+
+// Edit existing grid elements to be white
+function clearGrid(){
+    for(myRow of grid.children){
+        for(element of myRow.children){
+            element.classList.add("blockTransition");
+            element.style.backgroundColor = "white";
+        }
+    }   
+}
 
 function buildGrid(size){
     for(let x = 0; x < size; x++){
@@ -35,11 +75,18 @@ function buildGrid(size){
     }
 }
 
-function clearGrid(){
+
+// Remove elements attached to grid
+function removeGrid(){
     grid.replaceChildren();
     row.replaceChildren();
 }
 
-size = sizeSelect.value;
+function generateRainbowColor(){
+    return `rgb(${generateRandomInteger(257)}, ${generateRandomInteger(257)}, ${generateRandomInteger(257)}`
+}
 
-buildGrid(size);
+// Generate a number between 0 and max
+function generateRandomInteger(max) {
+    return Math.floor(Math.random() * max);
+}
